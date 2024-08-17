d9fea633f87a220bf85aea5ff45f60c3b00d5f2c
Fraylin <fraylinayala321@gmail.com>
2024-08-17 02:18:33 -0400

    Updated main.js and created sharedData.js to share SubscriptionsArray accross js files

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let subscriptionsNumber = document.getElementById("subscriptionsNumber");
+const subscriptionsNumber = document.getElementById("subscriptionsNumber");
+const subscriptionsArray = [];
 subscriptionsNumber.addEventListener("change", onChange);
-let userInputSubscriptions=[];
 let temp;
 let subscriptionJSON = {
   "subscriptions": [
@@ -11,7 +11,7 @@ let subscriptionJSON = {
           "price": "$8",
           "ads": "No ads",
           "library": "100,000 movies and TV Shows",
-          "anime": "Few Tittles; mostly dubbed",
+          "anime": "Few Titles; mostly dubbed",
         },
        "hulu": {
           "name": "Hulu",
@@ -89,14 +89,13 @@ let subscriptionJSON = {
 };
 
 
-let emptySubscriptionList = [];
+
 
 let button = document.getElementById("submitButton");
 let subscriptionsNumberValue = 0;
 
-button.addEventListener("click", onClick);
+button.onclick = buttonClick;
 
-document.querySelector("#subscriptionContainer1").style.display = "none";
 document.querySelector("#subscriptionContainer2").style.display = "none";
 document.querySelector("#subscriptionContainer3").style.display = "none";
 document.querySelector("#subscriptionContainer4").style.display = "none";
@@ -106,113 +105,23 @@ document.querySelector("#subscriptionContainer7").style.display = "none";
 document.querySelector("#subscriptionContainer8").style.display = "none";
 document.querySelector("#subscriptionContainer9").style.display = "none";
 document.querySelector("#subscriptionContainer10").style.display = "none";
-document.querySelector("#submitButton").style.display= "none";
 
-function onClick(module) {
-  subscriptionsNumberValue = document.getElementById(
-    "subscriptionsNumber"
-  ).value;
-  if (subscriptionsNumberValue == 1) {
-    userInputSubscriptions=[]
-    userInputSubscriptions.push(document.getElementById("subscription1").value)
-    console.log(userInputSubscriptions);
-  }
-  else if(subscriptionsNumberValue == 2){
-    userInputSubscriptions=[]
-    temp= document.querySelectorAll(".subscriptions");
-    for (var i=0;i<2;i++){
-      userInputSubscriptions.push(temp[i].value);
-    }
-    console.log(userInputSubscriptions);
-    
-    
-    
-    
-  }
-  else if(subscriptionsNumberValue == 3){
-    userInputSubscriptions=[]
-    temp= document.querySelectorAll(".subscriptions");
-    console.log(temp);
-    for (var i=0;i<3;i++){
-      userInputSubscriptions.push(temp[i].value);
-    }
-    console.log(userInputSubscriptions);
-    
-    
-  }
-  else if(subscriptionsNumberValue == 4){
-    userInputSubscriptions=[]
-    temp= document.querySelectorAll(".subscriptions");
-    console.log(temp);
-    for (var i=0;i<4;i++){
-      userInputSubscriptions.push(temp[i].value);
-    }
-    console.log(userInputSubscriptions);
-   
-   
-  }
-  else if(subscriptionsNumberValue == 5){
-    userInputSubscriptions=[]
-    temp= document.querySelectorAll(".subscriptions");
-    console.log(temp);
-    for (var i=0;i<5;i++){
-      userInputSubscriptions.push(temp[i].value);
-    }
-    console.log(userInputSubscriptions);
-    
-   
-    
-  }
-  else if(subscriptionsNumberValue == 6){
-    userInputSubscriptions=[]
-    temp= document.querySelectorAll(".subscriptions");
-    console.log(temp);
-    for (var i=0;i<6;i++){
-      userInputSubscriptions.push(temp[i].value);
-    }
-    console.log(userInputSubscriptions);
+
+function buttonClick() {
+  const subscriptionsNumberValue = document.getElementById("subscriptionsNumber").value;
   
-  }
-  else if(subscriptionsNumberValue == 7){
-    userInputSubscriptions=[]
-    temp= document.querySelectorAll(".subscriptions");
-    console.log(temp);
-    for (var i=0;i<7;i++){
-      userInputSubscriptions.push(temp[i].value);
-    }
-    console.log(userInputSubscriptions);
+
   
+  for (let i = 1; i <= subscriptionsNumberValue; i++){
+    let currentSubscription = {
+      name : document.getElementById("subscription"+i).value,
+      price: document.getElementById("price"+i).getElementsByTagName("input")[0].value
+    };
+
+    subscriptionsArray.push(currentSubscription);
   }
-  else if(subscriptionsNumberValue == 8){
-    userInputSubscriptions=[]
-    temp= document.querySelectorAll(".subscriptions");
-    console.log(temp);
-    for (var i=0;i<8;i++){
-      userInputSubscriptions.push(temp[i].value);
-    }
-    console.log(userInputSubscriptions);
-   
-  }
-  else if(subscriptionsNumberValue == 9){
-    userInputSubscriptions=[]
-    temp= document.querySelectorAll(".subscriptions");
-    console.log(temp);
-    for (var i=0;i<9;i++){
-      userInputSubscriptions.push(temp[i].value);
-    }
-    console.log(userInputSubscriptions);
-    
-  }
-  else if(subscriptionsNumberValue == 10){
-    userInputSubscriptions=[]
-    temp= document.querySelectorAll(".subscriptions");
-    console.log(temp);
-    for (var i=0;i<10;i++){
-      userInputSubscriptions.push(temp[i].value);
-    }
-    console.log(userInputSubscriptions);
-    
-  }
+ 
+  
 }
 
 function onChange() {
