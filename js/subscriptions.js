ef06ed8ced4b89c9ca5076572f57335fe51d885d
Fraylin <fraylinayala321@gmail.com>
2024-09-04 22:30:40 -0700

    Fixes on the subscriptions.html page

--- js/subscriptions.js
+++ js/subscriptions.js
@@ -3,15 +3,31 @@ let number = localStorage.getItem('number')
 
 
 for (let i=0; i < number; i++){
-    console.log(array[i]);
-    const liElement = document.createElement("li");
     const divElement = document.createElement("div");
-    const leftSide = document.getElementById("")
-    const textNode = document.createTextNode(array[i].name + ": $" + array[i].price + "\t");
-    liElement.style.marginLeft= '10px';
-    liElement.appendChild(textNode);
+    const imgElement = document.createElement("img");
+    const pElement = document.createElement("p");
+    const leftSide = document.getElementById("left");
+    const rightSide = document.getElementById("right");
+    if (array[i].status == "active"){
+        pElement.innerHTML = "You are currently subscribed to " + array[i].name + " paying " + array[i].price;
+        pElement.id = "subsText";
+        imgElement.src = "html/images/"+ array[i].name + ".png";
+        imgElement.id = "slogo";
+        divElement.append(pElement);
+        leftSide.appendChild(divElement);
 
-    const subList = document.getElementById("subscriptionsList");
-    subList.style.padding = 'auto';
-    subList.appendChild(liElement);
+
+
+    }
+
+    
+    rightSide.appendChild(divElement);
+
+    divElement.append(imgElement);
+    divElement.append(pElement);
+
+
+
+    const textNode = document.createTextNode(array[i].name + ": $" + array[i].price + "\t");
+
 }
