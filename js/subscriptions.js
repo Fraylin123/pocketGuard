3070c7c4bab28879adb0954558f52c481773be9a
Fraylin <fraylinayala321@gmail.com>
2024-09-04 23:50:41 -0700

    Added more images and fixed the logic of the subscriptions.js file

--- js/subscriptions.js
+++ js/subscriptions.js
@@ -2,32 +2,47 @@ let array = JSON.parse(localStorage.getItem('subscriptionsArray'));
 let number = localStorage.getItem('number')
 
 
+const leftSide = document.getElementById("left");
+const rightSide = document.getElementById("right");
+
 for (let i=0; i < number; i++){
+    
     const divElement = document.createElement("div");
     const imgElement = document.createElement("img");
     const pElement = document.createElement("p");
-    const leftSide = document.getElementById("left");
-    const rightSide = document.getElementById("right");
-    if (array[i].status == "active"){
-        pElement.innerHTML = "You are currently subscribed to " + array[i].name + " paying " + array[i].price;
+    
+    if (array[i].status == "Active"){
+        console.log("Inside")
         pElement.id = "subsText";
+        pElement.innerHTML = "You are currently subscribed to " + array[i].name + " paying $" + array[i].price;
         imgElement.src = "html/images/"+ array[i].name + ".png";
         imgElement.id = "slogo";
+        
+        divElement.id = "subs";
+        divElement.append(imgElement)
         divElement.append(pElement);
+        
         leftSide.appendChild(divElement);
 
-
+    }
+    else if (array[i].status == "Inactive"){
+        pElement.id = "unsubsText";
+        pElement.innerHTML = "You are not subscribed to " + array[i].name + " ($" + array[i].price + ")";
+        imgElement.src = "html/images/"+ array[i].name + ".png";
+        imgElement.id = "slogo";
+        
+        divElement.id = "unsubs";
+        divElement.append(imgElement);
+        divElement.append(pElement);
+        
+        rightSide.appendChild(divElement);
 
     }
 
     
-    rightSide.appendChild(divElement);
 
-    divElement.append(imgElement);
-    divElement.append(pElement);
+    
+   
 
-
-
-    const textNode = document.createTextNode(array[i].name + ": $" + array[i].price + "\t");
 
 }
